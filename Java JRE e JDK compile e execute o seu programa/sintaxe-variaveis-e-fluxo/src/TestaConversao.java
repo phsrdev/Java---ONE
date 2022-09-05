70e7d6f1375d224cebab21c0643251f34e48fbf7
Pedro Rodrigues <phsr@live.com>
2022-09-04 23:29:40 -0300

    Final da aula 4

---
+++ Java JRE e JDK compile e execute o seu programa/sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,17 @@
+
+public class TestaConversao {
+
+	public static void main(String[] args) {
+        double salario = 1270.50;
+        int valor = (int) salario;
+        System.out.println(valor);
+        long numeroGrande = 32432423523L;
+        short valorPequeno = 2131;
+        byte b = 127;
+        System.out.println("long " + numeroGrande + " short " + valorPequeno + " byte " + b);
+        double valor1 = 0.2;
+        double valor2 = 0.1;
+        double total = valor1+valor2;
+        System.out.println(total);
+    }
+}
---
+++ Java JRE e JDK compile e execute o seu programa/sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,21 @@
+public class TestaVariaveis {
+
+    public static void main(String[] args) {
+        System.out.println("ola novo teste");
+
+        int idade;
+        idade = 30;
+
+        System.out.println(idade);
+
+        idade = 20 + 10;
+
+        System.out.println(idade);
+
+        idade = (8 * 3) +  6;
+
+        System.out.println(idade);
+        
+        System.out.println("a idade é " + idade);
+    }
+}
