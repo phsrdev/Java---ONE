e666a1dec329213592d896a7d1d1a8ec91e01c38
Pedro Rodrigues <phsr@live.com>
2022-10-12 17:58:51 +0100

    Final aula 2

--- Java e java.util Coleções, Wrappers e Lambda expressions/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TestArrayReferencias.java
+++ Java e java.util Coleções, Wrappers e Lambda expressions/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TestArrayReferencias.java
@@ -1,25 +1,33 @@
 package br.com.bytebank.banco.test;
 
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TestArrayReferencias {
 	
 	public static void main(String[] args) {
 		
 		//int[] idades = new int[5];
-		ContaCorrente[] contas = new ContaCorrente[5];
+		Object[] referencias = new Object[5];
+		
 		ContaCorrente cc1 = new ContaCorrente(22, 11);
-		contas[0] = cc1;
+		referencias[0] = cc1;
 		
-		ContaCorrente cc2 = new ContaCorrente(22, 22);
-		contas[1] = cc2;	
+		ContaPoupanca cc2 = new ContaPoupanca(22, 22);
+		referencias[1] = cc2;	
 		
+		Cliente cliente = new Cliente();
+		referencias[2] = cliente;
 		
 		//System.out.println(cc2.getNumero());
-
-		System.out.println( contas[1].getNumero()  );
 		
-		ContaCorrente ref = contas[1];
+//		Object referenciaGenerica = contas[1];
+//		
+//		System.out.println( referenciaGenerica.getNumero()  );
+		
+		ContaPoupanca ref = (ContaPoupanca) referencias[1];//type cast
 		System.out.println(cc2.getNumero());
 		System.out.println(ref.getNumero());
 		
